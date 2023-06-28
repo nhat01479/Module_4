863f8e371da6defda6dd8d475dc02baa337a27f6
nhat01479 <nhat01479@gmail.com>
2023-06-28 15:39:17 +0700

    Cắt template

--- SpringGreeting/src/main/java/controllers/HelloController.java
+++ SpringGreeting/src/main/java/controllers/HelloController.java
@@ -1,2 +1,36 @@
-package controllers;public class HelloController {
+package controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+/*URI Pattern
+?  - trùng khớp với 1 ký tự
+*  - trùng khớp với 0 hoặc nhiều ký tự
+** - trùng khớp với 0 hoặc nhiều phần của đường dẫn (path
+segment)
+*/
+@Controller
+public class HelloController {
+
+    @RequestMapping("/car?/s?o?/info1")
+    public String info1(Model model) {
+        model.addAttribute("message", "Info1");
+        return "/index";
+    }
+
+    @RequestMapping("/c*/s*d/info2")
+    public ModelAndView info2() {
+        ModelAndView model = new ModelAndView("/index");
+        model.addObject("message", "Info2");
+        return model;
+    }
+
+    @RequestMapping("/card/**")
+    public ModelAndView info3() {
+        ModelAndView model = new ModelAndView("/index");
+        model.addObject("message", "Info3");
+        return model;
+    }
 }
--- SpringGreeting/src/main/webapp/WEB-INF/views/index.jsp
+++ SpringGreeting/src/main/webapp/WEB-INF/views/index.jsp
@@ -5,5 +5,6 @@
 </head>
 <body>
 <h1>Hello ${name}!</h1>
+<h2>${message}</h2>
 </body>
 </html>
